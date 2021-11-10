7f9a2524b3b0fb21a14db306ff222956cd1e38fc
adarsh-quleep <adarsh.kumar@arnxt.com>
2021-11-10 12:13:28 +0530

    Blog page updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-
 import {About,Blog,ContactUs,Faqs,Features, LandingPage,Navbar,Pricing,Privacypolicy,Products,Footer,WhyBest,TermsAndConditions,Subscribe} from "./Component"
 import {
   BrowserRouter as Router,
@@ -74,25 +73,4 @@ function App() {
 
 export default App;
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
-// export default App;
--- src/Component/Blog.js
+++ src/Component/Blog.js
@@ -1,50 +1,126 @@
-import React from 'react';
-import { Link } from "react-router-dom";
+import React, {useEffect} from 'react';
+import {Link} from 'react-router-dom';
 
 function blog() {
+
     return (
-        <div>
+      <div>
+        <div
+        id="privacy"
+        className="section section-padding offwhite-bg mt-5 p-5"
+      >
+        <div className="container text-start">
+          {/* heading start */}
+          <div className="">
+            <h3 className="text-center">Blogs</h3>
+            <br/>
+            <br/>
+            <h3 className="text-center">Opportunities for AR in Retail Market</h3>
+          </div>
+          {/* heading end */}
+          {/* description start */}
+          <div name="description">
+            <p className="section-subtext">
+            Retailers are instead turning to AR to help customers digitally test out 
+                 thousands of beauty products to assist in buying decisions. <br/>
+                 Launched six years ago, Ulta’s virtual try-on beauty tool, GLAMlab, has seen a surge in usage since the pandemic. Engagement has increased seven-fold, and more than 50 million shades of foundation have been swatched digitally with the app post-Covid.
+According to a Neilsen global survey from 2019, consumers listed Augmented and Virtual Reality as the top technologies they’re seeking to assist them in their daily lives. In fact, just over half (51%) said they were willing to use this technology to assess products. Thus, Totally expecting that the interest has since soared as we’ve seen AR shift from being sometimes gimmicky to now solving real pain points for customers, especially amid the pandemic. In fact, e-commerce company ‘Shopify’ recently released new data that interactions with products having AR content showed a 94% higher conversion rate than products without AR.
+Retailers are also beginning to use AR technology to reimagine the digital shopping experience with virtual storefronts. In May, retailer Kohl’s collaborated with Snapchat to create Kohl’s AR Virtual Closet. Using a smartphone and the Snapchat app, consumers can step inside an AR dressing room, mix and match items, and make a purchase without ever leaving their home.
+<br/>
+The next phase of augmented retail will likely be a gamified social experience.Alsol ‘Burberry’ partnered with Snapchat on an in-store AR game, and I can see the concept being extended to digital storefronts and virtual closets where you can play, explore, and shop with friends. This coincides with a current trend popular among fashion and beauty brands such as Estée Lauder, Gucci, and Miu Miu: mobile arcade games. Burberry’s “B Surf'' mobile racing game even featured AR face filters and characters as prizes. 
+<br/> <br/>
+Fashion and beauty companies applying this digital entertainment approach are benefitting by connecting with new, younger consumers. 
+Which leads us to another emerging area in augmented retail and digital shopping: virtual goods as commodities. We’re already seeing the sale of virtual merchandise from luxury retailers like Louis Vuitton offering digital skins (branded clothing and accessories to dress characters with) in the esports game League of Legends. Consumer spending on gaming loot boxes and skins worldwide is predicted to hit $50 billion (USD) by 2022.
+Virtual try-on experiences are an excellent use case for AR in retail: allowing consumers to preview products to scale digitally in their own homes, on their own bodies, and then instantly purchasing the corresponding physical product. But what if in addition to physical items, you could buy virtual objects, such as jewelry, apparel, or art, for which there may or may not be a physical counterpart? 
+Predicting a rise in the sale of virtual goods with a new type of augmented retail. Impacted by the pandemic, the “digital lipstick effect” is a trend. The “lipstick effect” has historically referred to consumers continuing to spend on small luxury items. Lipstick as a potentially accessible product becomes a metaphor in today’s times, analogous to digital lipstick, or any virtual good.
+Physical retail must evolve in response, and AR has proven that it can add enormous value for consumers in the shopping journey. Now is the time for business leaders and brands to not only reimagine retail, but to catapult these immersive shopping experiences into the future.
+            <br/>
+            <br/>
+            <h3 className="text-center">Difficulties:
+Lack of Proven Business Models
+</h3>
+
+One of the weirdest things about Augmented Reality technology is that despite experiencing broad adoption and mass public acceptance - it is not doing all that well business-wise.
+Let's dive deep into the reason for the lack of industry growth in the field. There is a steady flow of investments in the augmented reality app market, and the general background is more than positive. However, all these investments are yet to pay off big time. Part of the reason is that no one has figured out a distinct AR-related Business Model that will work long-term; besides, probably, the gaming industry.
+Sure, startups are popping up with more and more outlandish concepts at hand and many big companies are trying out various AR-related solutions, but there is one thing that should be noted - all these solutions are integrated into the business models that can be effective with or without an AR solution.
+<br/> <br/>
+The biggest AR product of recent times - Pokemon Go broke even only because of the power of the brand. After the hype died down, the retention rate followed. The same goes for IKEA and Amazon AR applications - they are fun, but you can live without them. 
+However, there is a ray of light in the form of industrial companies. Why? They have enough financial resources to boost development, and there are plenty of fields where AR technology might come at handy.
+Let’s name a few:
+Proof of Concept for construction projects; Discrepancy Checking; Construction Progress Monitoring; Hidden infrastructure visualization; Maintenance Instructions;
+These kinds of applications have the potential to establish an augmented reality technology not as a toy but as a viable helping tool that improves the overall quality of the end product
+One of the best hardware startup examples of this situation where the challenge was successfully navigated is Fitbit. Though there have been many IoT sensor products designed for different applications including fitness, Fitbit managed to not only innovate on the product offering but it also found a sustainable business model to scale revenue, helping them cross the chasm of innovation to reach mainstream customers. But why is creating a great business model so difficult? Why don’t more startups start first with the perfect business model and then seek technology innovation? And why is it often the second or third waves of startups which have a higher success rate than previous ones?
+All of these questions are valid and require a deep dive into the obstacles associated with being a pioneer leading a new industry. In particular at my startup, we have learned a lot in our journey and still have not crossed the chasm. Yet I would like to share some of the lessons that have come  with being early in VR, AR and AI with both hardware and software.
+<br/> <br/>
+<h3 className="text-center">Lack of Augmented Reality App Design & Development Standards</h3>
+
+tandards are something of a universal language for a software application. It is one of the ways to secure its compatibility and contribution to the overall development of the technology. At the moment, this is the thing that is under construction for Augmented Reality.
+The reason is simple - it's too soon. The technology is too new, and it is still coming to its own both in hardware and software terms (despite "technically" being around for a while.)
+So what’s the problem? Without standards, every augmented reality-related project is a thing of its own barely compatible with the others. That complicates the process of unifying solutions to the greater whole which makes the overall development of the technology much slower than it could have been if everyone had been on the same page.
+However, implementation of technical standards is a question of time, and its adoption will signify the final stage of establishing the technology as a real deal. 
+ 
+<br/>
+<br/>
+<h3 className="text-center">Security & Privacy Issues with Augmented Reality</h3>
+
+
+Privacy & Security also pose significant challenges for the AR industry. Due to inconsistencies in augmented reality programming, oversight, and negligence, there is a legitimate chance of getting into trouble without meaning to do so.
+The biggest issue is that no actual regulation designates what is allowed and what is not in the augmented reality environment. This means the technology can be used with malicious intent just as it can be used for entertainment.
+For example, the “try before you buy” option for clothing, but instead of overlaying the cloth on your body someone may overlay another nude body and spread it to damage your reputation or blackmail. Or AR can be used to hijack accounts via surveillance and mining data output by slightly manipulating and overlaying AR content (just as in ad stacking fraud schemes).
+Part of the problem is a lack of awareness about these problems. People don’t understand how sensitive the subject is. The other part of the problem is the reluctance of the developers to take action before there is any heat on the corner.
+
+
+
+<h3 className="text-center">The Possibility of Physical Harm</h3>
+While long-term effects of using Augmented reality are much better documented than ones for Virtual Reality (uh-hm), there is still a significant possibility of harming yourself and the surroundings due to the nature of the application and lack of attention.
+The thing is - Augmented Reality operates in the real world and adds a little bit of digital into it. These elements are driving attention away from reality which may cause a potentially dangerous situation.
+For example, remember all the news about people hurting themselves while playing Pokemon Go? Well, it is just the tip of an iceberg.
+Augmented Reality, while being a worthwhile addition to the proceedings, can also serve as a significant distracting factor. This is why automobile manufacturers are so reluctant with implementing AR displays - it is more of a liability than the actual advantage for the driving process.  
+As such, there is a need to develop a certain kind of AR interface that will be useful and yet not distracting from the process.
+
+<br/>
+<br/>
+<h3 className="text-center">Poor Quality of Content & Use Cases</h3>
+
+While AR technology is rapidly developing and gradually expanding its scope - the problem with its use cases and the content remains. It is currently leading the top of unsolved problems in augmented reality in terms of being threatening to derail an industry.
+The majority of AR-related content available at App Stores is mostly a showcase of simple tricks and not much else. Its goal is to present some brands as forward-looking and cunning. It is designed to be a one-off affair.
+Sure, AR technology is still in its infancy, but if you look at what augmented reality development companies are trying to pull off with more or less the same tools (all HoloLens or Google Cardboard projects related to Healthcare or Education) - the solutions above seem a bit lacking.
+Part of the reason for this problem is because of a lack of expertise in the field. There are not enough developers who have a firm grasp on technology and can deliver an accessible and useful experience.
+The other big problem looming over the AR industry is the lack of credible use cases for AR technology. At the moment, the majority of AR apps are one-trick ponies that can do one thing which can be done without their assistance with the same level of effectiveness.
+Augmented Reality is an additional element to other activities by design - it must significantly contribute to the process either by providing additional information more conveniently or providing assistance in performing specific actions.
+Part of the reason why it is so is that of the very nature of AR - it is an addition, augmentation of the other thing, and the only way for it to be successful is to be a natural extension that makes the overall activity easier and more effective, which comes with tries and fails.
+AR displays for cars seems like a good idea on paper, but not a useful solution in practice. On the other hand, AR in medical or engineering training can be very helpful. The same goes for infrastructure planning and on-set simulations.
+<br/>
+<br/>
+<h3 className="text-center">Social Issues of Augmented Reality: Public Acceptance & Retention</h3>
+While Augmented Reality seems to be a relatively popular topic in the media and frequently mentioned as one of the most exciting emerging technologies - its overall public reception is, for the lack of a better word, mild.
+Part of the reason for that is that the quality of the majority of AR content is mostly hit and miss. Out of this comes the notion that AR applications are nothing more than an unnecessary addition. However, the situation will probably revert with a couple of “killer apps” that will prove the worth of technology in one swift sweep.
+The other part of the problem is that the public, for the most part, is not aware of the benefits of augmented reality in various fields. It is still perceived as zany science fiction by a significant portion of potential users.
+Then there is a retention problem. Despite the popularity of Augmented Reality technology steadily growing over the past few years, there is still a significant problem in keeping an audience using Augmented Reality applications in a long-term perspective.
+The current state of affairs with AR apps looks like this: users download the app because they saw an advert that excited them a couple of times until the goal is realized or interest is satisfied and then abandon the app due to lack of long-term use cases.
+<br/>
+<br/>
+<h3 className="text-center">Augmented Reality Technology Problems & Limitations</h3>
+Another big problem with implementing AR solutions is the technological gap between AR devices. It is one thing to design an app for a fully-fledged AR gear, and it is a completely different thing to do it for a smartphone. The latter case got many limitations that make the whole experience not really user-friendly and somewhat redundant to the activity it augments.
+Considering that the majority of the target audience will not likely purchase AR gear due to its impractical and high prices - smartphones remain a preferred function and since they have certain augmented reality app design limitations - it neuters the whole point of implementing AR solution to the mix.
+What’s the solution to this problem? It is a question of time when the price for AR gear will drop to a mass consumer acceptable level. The thing is - Augmented Reality Technology is in its early stages, and it is too soon to expect that its gear will be available for a regular Joe from the get-go.
+<br/>
+<br/>
+<h3 className="text-center">Ethical and Legal Issues with Augmented Reality</h3>
+The main ethical challenges in terms of AR implementation include facial recognition and anonymity, mental and social side effects, unrealistic expectations, reality distortion, and manipulation. These ethical considerations need to be taken into account in terms of AR. Currently, there is no regulatory infrastructure in place to moderate the development and deployment of AR technology. What is even more worrying is that the speed at which technology is advancing is too fast for the traditional legislative system to account for. Blockchain technology is an example of this. As soon as a bill is passed new features have been added and created rendering the bill more or less obsolete.
+The control over AR software should be in the hands of customers so they can make the decisions with regard to what they want to see and what data is made accessible. Like all technology, AR is a two-edged sword and needs to be correctly wielded in order to be effective. With high power comes great responsibility and AR is such a power. While it is fantastic that you can easily navigate through the streets using your augmented reality app, public spaces become a sort of "hijacked." Remembering the Pokemon Go and the rather sensitive story about how people go to the Holocaust Museum to catch "that one pokemon" they need for the collection? 
+It is also important to remember that when people are using augmented reality apps, they are sort of "located" in two worlds - one real and another virtual. Therefore, as we mentioned above, you should beware of where you're located in the physical world, making sure you're not about to cross a busy road with your nose in the cell phone or wander into someone's private land ; and get a fine, in the best-case scenario.
+            </p>
             
-
-    <div className="post">
-      {/* <img
-        className="postImg"
-        src='assets/images/logo7.png'
-        alt=""
-      /> */}
-      <h2>Blogs</h2>
-      <div className="postInfo">
-        <div className="postCats">
-          <span className="postCat">
-            <Link className="link" to="/">
-              Music
-            </Link>
-          </span>
-          <span className="postCat">
-            <Link className="link" to="/">
-              Life
-            </Link>
-          </span>
+              
+            </div>
+          </div>
+    
         </div>
-        <span className="postTitle">
-          <Link to="/post/abc" className="link">
-            Lorem ipsum dolor sit amet
-          </Link>
-        </span>
-        <hr />
-        
       </div>
-      <p className="postDesc">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda
-        officia architecto deserunt deleniti? Labore ipsum aspernatur magnam
-        fugiat, reprehenderit praesentium blanditiis quos cupiditate ratione
-        atque, exercitationem quibusdam, reiciendis odio laboriosam?
-      </p>
-    </div>
-  );
 
 
-        </div>
+      
     )
 }
 
